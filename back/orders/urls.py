83b70f810c6948e14f653c9d4d5441d6f831a35b
박기현 <kiryanchi@baggihyeons-Laptop.local>
2022-12-03 03:51:56 +0900

    orders views

--- back/orders/urls.py
+++ back/orders/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('orders/', views.orders, name='orders'),
     path('order/<int:order_id>/', views.order_r, name='order_r'),
-    path('order/<int:order_id>/', views.order_c, name='order_c'),
+    path('order/', views.order_c, name='order_c'),
     path('order/<int:order_id>/', views.order_d, name='order_d'),
 ]

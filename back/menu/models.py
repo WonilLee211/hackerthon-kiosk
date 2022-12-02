8dda9333ddbc66444cf676f432a305cba4c00cf8
박기현 <kiryanchi@172.20.nate.com>
2022-12-03 01:02:52 +0900

    media

--- back/menu/models.py
+++ back/menu/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Burger(models.Model):
+    name = models.TextField()
+    price = models.IntegerField()
+
+
+class Beverage(models.Model):
+    name = models.TextField()
+    price = models.IntegerField()
+
+
+class Side(models.Model):
+    name = models.TextField()
+    price = models.IntegerField()
